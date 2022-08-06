44866d6fecf0f018f69130c9e361fdfc049a851c
Alex <aleksejpinazik@gmail.com>
2022-08-06 17:30:41 +0300

    add new files

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="container box">
+        <div class="flex items-center pt-6">
+          <a href="/">
+          <img class="img-fluid" src="./assets/svg/logo.svg" alt="logo">
+          </a>
+          <MenuHeader />
+        </div>
+        <div class="">
+          <BusinessArea />
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import MenuHeader from './components/MenuHeader.vue';
+  import BusinessArea from './components/BusinessArea.vue';
+  import './assets/tailwind.css';
+
+  export default {
+    name: 'App',
+    components: {
+      MenuHeader,
+      BusinessArea
+    }
+  }
+</script>
---
+++ src/components/BusinessArea.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex w-full justify-end">
+       <div class="flex flex-col flex-1">
+        <div class="pt-44 max-w-xl sm:max-w-full">
+            <h1 class="text-[#0D0820] deliver-title font-bold pb-5">Deliver Your Business</h1>
+            <span class="text-base deliverin-text">Deliverin serves the needs of its loyal customers, with
+            domestic and international delivery services.</span>
+          </div>   
+        <div class="flex pt-16">
+            <a href="/" class="flex flex-col store-btn"><span class="available-title">Available on the</span><span class="font-bold">App Store</span></a>
+            <a href="/" class="flex flex-col store-btn mx-5"><span class="get-title">GET IT ON</span><span class="font-bold">Google Pay</span></a>
+            </div>
+        </div>
+            <div class="flex-1 pt-14">
+            <img src="../assets/images/exited.webp" alt="Deliver" class="img-fluid">
+            </div>
+       </div>
+</template>
